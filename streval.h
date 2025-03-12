47d5ae57bc91a9e14a5d55eb87d8cac103314dff
allets <48606593+alletsbckwrds@users.noreply.github.com>
2025-03-12 10:47:58 +0100

    Add files via upload

---
+++ streval.h
@@ -0,0 +1,35 @@
+//not the same as ch's streval(), this cannot remember variables. Also, you need to do this printf("%s",streval(...)) if you want to see any output at all
+
+char* streval(char* in) {
+	FILE* file = popen(in, "r");
+	if(file == NULL){return (char*) NULL;}
+	
+	size_t bufferSize=128;
+	char *buffer = (char*) malloc(bufferSize);
+	if(buffer == NULL){
+		pclose(file);
+		return (char*) NULL;
+	}
+
+	size_t totalCharsRead = 0;
+	size_t charsRead = 0;
+
+	while((charsRead = fread(buffer + totalCharsRead, 1, bufferSize - totalCharsRead - 1, file)) > 0){
+		totalCharsRead += charsRead;
+		if (totalCharsRead >= bufferSize -1){
+			bufferSize *= 2; //doubles the buffer if there is not enough space
+			char *temp = (char*) realloc(buffer, bufferSize); //does this work? its never been allocated before...
+			if(temp == NULL){
+				free(buffer); pclose(file); return (char*) NULL;
+			}
+			buffer=temp;
+		}
+	}
+
+	pclose(file);
+	buffer[totalCharsRead] = '\0';	
+
+	
+	return buffer;
+	
+}
